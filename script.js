ff1d65ab6a22fba9fa1722d3210a28c24761f45e
Karollayne802 <karollaynecorreia80@gmail.com>
2026-07-21 10:20:42 -0300

    Refactor weather fetching and display logic

--- script.js
+++ script.js
@@ -32,12 +32,48 @@ return data;
 }
 
 }
+
 //mostra as informações do clima
 async function showWeather(cidade) {
 
+    // Busca os dados com a API
     const dados = await getWeather(cidade);
 
-    const location = document.getElementsById("location");
+    // será exibido o nome da cidade
+    const location = document.getElementById("location");
+
+    //Mostra o nome da cidade retornado pela API
+    location.textContent = dados.name;
+
+    //Exibe a temperatura
+    const mainTemperature = document.getElementById("main-temperature");
+
+    // Mostra a temperatura retornada pela API
+    mainTemperature.textContent = `${dados.main.temp} °C `;
+
+    // Exibe a sensação térmica
+    const feelsLike = document.getElementById("feels-like");
+     feelsLike.textContent = `${dados.main.feels_like}°C`;
+
+     //Exibe a umidade do ar
+     const humidity = document.getElementById("humidity");
+     humidity.textContent = `${dados.main.humidity}%`;
+
+     //Exibe a velocidade do vento
+     const wind = document.getElementById("wind");
+     wind.textContent = `${dados.wind.speed} m/s`;
     
-    
+     //Exibe a rajada de vento
+     const windGust = document.getElementById("wind-gust");
+    windGust.textContent = `${dados.wind.gust} m/s`;
+
+    //Exibe o tipo do clima
+    const weatherMain = document.getElementById("weather-main");
+    weatherMain.textContent = dados.weather[0].main;
+
+    //Exibe o ícone do clima
+    const weatherIcon = document.getElementById("weather-icon");
+    weatherIcon.src = dados.weather[0].icon;
+
+
 }
